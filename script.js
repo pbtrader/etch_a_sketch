4821d059ffa3a709baa4969fa07e4140581ff717
Jacob Frieling <jacobfrieling@gmail.com>
2017-02-13 13:42:16 -0600

    new grid function updates

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 $(document).ready(function(){
-	for (var x = 0; x < 16; x++){
-		for (var y = 0; y < 16; y++){
-			var block = $("<div class='block'></div>");
-			$("#grid").append(block);
-		}
-	}
+	
+	newGrid(16);
+
 	$(document).on('mouseover','.block',function() {
-        $(this).css('background-color','green');
+        $(this).css('background-color','grey');
     });
 
     $('#reset').click(function(){
     	$('.block').css('background-color','white');
     })
+    
+    $('#changeGrid').click(function(){ 
+		var gridSize=prompt("Enter number of squares per size");
+		newGrid(gridSize);
+		var squareSize = (320/gridSize);
+       $('.block').css({'height': squareSize + 'px', 'width': squareSize + 'px'});
+    })
+
+    function newGrid(gridSize){
+    	var numSquares=gridSize * gridSize;
+    	$('#grid').empty();    	
+    	for (var j = 0; j < numSquares; j++){
+    		$("#grid").append("<div class='block'></div>");
+    		}
+    	}
+    
 });
+
+
+
+
